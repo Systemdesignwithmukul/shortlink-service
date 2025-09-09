d78eced600b21a14c2ac3622169a5137c50651fd
MUKUL20BCS2002 <cmukul2019@gmail.com>
2025-09-09 10:41:12 +0530

    shortlink: adding dependencies

---
+++ src/test/java/com/shortlink/ShortlinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortlinkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
